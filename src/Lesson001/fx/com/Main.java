e966bd0660d9dc8f182309fe797a5b47ac3f7701
vancent0228 <31526442+vancent0228@users.noreply.github.com>
2019-08-09 16:40:24 +0800

    leaning javafx at home

--- src/Lesson001/fx/com/Main.java
+++ src/Lesson001/fx/com/Main.java
@@ -1,15 +1,16 @@
-package lesson001.fx.com;
+package Lesson001.fx.com;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
-
+        launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle("這是第一個javafx程序");
+        primaryStage.show();
     }
 }
