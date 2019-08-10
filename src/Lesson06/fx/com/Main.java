39616a8ba953470249e26ae10931a8b4eb212559
vancent0228 <31526442+vancent0228@users.noreply.github.com>
2019-08-10 09:42:29 +0800

    leaning javafx at home

---
+++ src/Lesson06/fx/com/Main.java
@@ -0,0 +1,20 @@
+
+//JavaFX视频教程第6课screen类
+
+package Lesson06.fx.com;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+        primaryStage.setTitle("JavaFX视频教程第6课screen类");
+        primaryStage.show();
+    }
+}
